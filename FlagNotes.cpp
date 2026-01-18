c3a2f50d7182d07bd495adcb55e52ed14624e999
kayla-kubicke <161649341+kayla-kubicke@users.noreply.github.com>
2026-01-18 09:42:01 -0800

    Project Study Notes

---
+++ FlagNotes.cpp
@@ -0,0 +1,8 @@
+// Using class for notes; don't want to use markdown.
+// Notes on gcc flags.
+
+// ADD:
+// -Wall
+// -Wextra
+// -Wno-sign-compare
+// -Werror
---
+++ ValgrindNotes.cpp
@@ -0,0 +1,2 @@
+// Using class for notes; don't want to use markdown.
+// Not gcc, but wanted to include valgrind notes.
